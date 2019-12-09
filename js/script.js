611b2a6608a341792191fa436b0553e995b71199
darom.chhuon <darom.chhuon@PNC.LAN>
2019-12-09 09:20:31 +0700

    message project

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function getUrl(){
+    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    return url;
+}
+$(document).ready(function(){
+    requestApi();
+    $('#recipes').on('change',() =>{
+        var chooseRecipe = $('#recipes').val();
+        console.log(chooseRecipe);
+    })
+});
+
+function requestApi(){
+    $.ajax({
+        dataType : 'json',
+        url : getUrl(),
+        success:(data) => chooseRecipe(data.recipes),
+        error:() => console.log("error"),
+    });
+}
+var allData= [];
+function chooseRecipe(recip){
+    allData = recip;
+    var option = "";
+    recip.forEach(item =>{
+        option +=`
+            <option value="${item.id}">${item.name}</option>
+        `;
+    });
+    $('#recipes').append(option);
+}
